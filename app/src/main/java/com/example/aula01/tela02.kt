3e793c18dc1a887ce15fff5a8bd5b4b8b0c9a62d
Guilherme-520 <guilhermecordeiro520@outlook.com>
2022-08-05 18:05:15 -0300

    segundo commit

---
+++ app/src/main/java/com/example/aula01/tela02.kt
@@ -0,0 +1,11 @@
+package com.example.aula01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela02)
+    }
+}
